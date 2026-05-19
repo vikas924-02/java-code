a9a7f989f8e277d449411af600c1ca7bf41f9bdc
vikas924-02 <nitinverma83218@gmail.com>
2026-05-19 11:48:20 +0530

    Add files via upload

---
+++ VowelConsonant.java
@@ -0,0 +1,25 @@
+public class VowelConsonant {
+    public static void main(String[] args) {
+
+        String str = "Programming";
+
+        int vowels = 0;
+        int consonants = 0;
+
+        str = str.toLowerCase();
+
+        for(int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowels++;
+            } else if(ch >= 'a' && ch <= 'z') {
+                consonants++;
+            }
+        }
+
+        System.out.println("Vowels = " + vowels);
+        System.out.println("Consonants = " + consonants);
+    }
+}
