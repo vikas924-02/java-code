aa4384bd8b6d0240563c3e6bb0b04828ab72b7c1
vikas924-02 <nitinverma83218@gmail.com>
2026-05-19 11:45:23 +0530

    Add files via upload

---
+++ Reversemethod.java
@@ -0,0 +1,11 @@
+public class Reversemethod {
+    static int sum(int[] arr, int n) {
+        if (n == 0) return 0;
+        return arr[n - 1] + sum(arr, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(sum(arr, arr.length));
+    }
+}
