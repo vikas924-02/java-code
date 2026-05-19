793e89cf01e5ae9c10eb6b109e3d728e2218922c
vikas924-02 <nitinverma83218@gmail.com>
2026-05-19 11:41:14 +0530

    Add files via upload

---
+++ BitManipulationStyle.java
@@ -0,0 +1,12 @@
+public class BitManipulationStyle {
+    public static void main(String[] args) {
+        int n = 5; 
+        
+        System.out.println("Original number: " + n);
+        
+        
+        int shifted = n << 1; 
+        
+        System.out.println("After left shift (n << 1): " + shifted);
+    }
+}
