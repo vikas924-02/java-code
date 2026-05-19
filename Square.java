7e6d6c741a30fbff5a029984ce14251900126080
vikas924-02 <nitinverma83218@gmail.com>
2026-05-19 11:56:14 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,17 @@
+public class Square {
+    public static void main(String[] args) {
+
+        int n = 3;
+
+        for(int i = 0; i < n; i++) {
+
+            for(int j = 0; j < n; j++) {
+
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
+//done
