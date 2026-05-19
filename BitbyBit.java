21cff2cfd9fecb20df3c3a116a581ba1e2a5da9a
vikas924-02 <nitinverma83218@gmail.com>
2026-05-19 11:52:22 +0530

    Add files via upload

---
+++ BitbyBit.java
@@ -0,0 +1,9 @@
+class BitbyBit{
+    public static void main(String[] args) {
+        int a = 9 , b = 10;
+        System.out.println(a/b); System.out.println(a<<2);
+        System.out.println(a&b); System.out.println(b>>1);
+        System.out.println(a^b); System.out.println(b>>2);
+        System.out.println(a<<1);
+    }
+}  
